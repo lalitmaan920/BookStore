bd0d46c64ec6fe92216719aadfd211a6b14bcfa2
lalitmaan920 <lalitmaan2004@gmail.com>
2025-07-11 11:58:03 +0530

    vercel file

--- frontend/src/Components/Banner.jsx
+++ frontend/src/Components/Banner.jsx
@@ -1,52 +1,45 @@
 import React from "react";
-
-const Banner = () => {
+import BannerImg from "../../public/Banner.png";
+function Banner() {
   return (
     <>
-      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-10">
-        <div className="order-2 md:order-1 w-full md:w-1/2 mt-12 md:mt-32">
-          <div className="space-y-10">
-            <h1 className="text-4xl font-bold">
+      <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-10">
+        <div className="w-full order-2 md:order-1 md:w-1/2 mt-12 md:mt-36">
+          <div className="space-y-8">
+            <h1 className="text-2xl md:text-4xl font-bold">
               Hello, welcomes here to learn something{" "}
-              <span className="text-green-500">new everyday!!!</span>
+              <span className="text-pink-500">new everyday!!!</span>
             </h1>
-            <p className="text-xl">
+            <p className="text-sm md:text-xl">
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
               et totam. Tempora amet atque expedita, quae corrupti totam sed
-              pariatur corporis at veniam est volutas animi!
+              pariatur corporis at veniam est voluptas animi!
             </p>
-            <label className="input validator rounded-full">
+            <label className="input input-bordered flex items-center gap-2">
               <svg
-                className="h-[1em] opacity-50"
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
               >
-                <g
-                  strokeLinejoin="round"
-                  strokeLinecap="round"
-                  strokeWidth="2.5"
-                  fill="none"
-                  stroke="currentColor"
-                >
-                  <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-                </g>
+                <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+                <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
               </svg>
-              <input type="email" placeholder="mail@site.com" required />
+              <input type="text" className="grow" placeholder="Email" />
             </label>
-            <div className="validator-hint hidden">
-              Enter valid email address
-            </div>
           </div>
-          <button className="btn btn-secondary rounded-full border-[2px]">Secondary</button>
+          <button className="btn mt-6 btn-secondary">Get Started</button>
         </div>
-        <div className=" order-1 md:order-2 w-full md:w-1/2 mt-12 md:mt-32 ml-50">
-        <img src="/banner.jpg" className="w-92 h-92" alt="Banner" />
+        <div className=" order-1 w-full mt-20 md:w-1/2">
+          <img
+            src={BannerImg}
+            className="md:w-[550px] md:h-[460px] md:ml-12"
+            alt=""
+          />
         </div>
       </div>
-      <br/>
     </>
   );
-};
+}
 
 export default Banner;
